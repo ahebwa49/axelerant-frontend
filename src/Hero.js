0b8e6976a9a41f42950ad8272fbd54f94131eeb2
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-11-14 20:09:56 +0300

    design the hero

---
+++ src/Hero.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Hero.css";
+import getInTouchIcon from "./static/img/getInTouchIcon.png";
+import serviceIcon from "./static/img/service.png";
+import serviceIconOne from "./static/img/service1.png";
+import serviceIconTwo from "./static/img/service2.png";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__header">
+        <div className="hero__header__logo">
+          COMPANY <span>LOGO</span>
+        </div>
+        <div className="hero__header__options">
+          <span>About</span>
+          <span>Services</span>
+          <span>Solutions</span>
+          <span>Blog</span>
+          <span>
+            <img
+              src={getInTouchIcon}
+              alt="get in touch"
+              className="hero__header__options__img"
+            />
+            <span className="hero__header__options__getintouch">
+              Get In Touch
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className="hero__heading">
+        <span>Integer bibendum sit amet arcu vel egestas</span>
+      </div>
+      <div className="hero__services">
+        <div className="hero__services__service">
+          <img src={serviceIcon} width="60" alt="service" />
+          <div className="hero__services__service__name">
+            <span>Service Name</span>
+          </div>
+        </div>
+        <div className="hero__services__service">
+          <img src={serviceIconOne} width="60" alt="service" />
+          <div className="hero__services__service__name">
+            <span>Service Name</span>
+          </div>
+        </div>
+        <div className="hero__services__service">
+          <img src={serviceIconTwo} width="60" alt="service" />
+          <div className="hero__services__service__name">
+            <span>Service Name</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
